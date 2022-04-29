f0093ae433e3800193634e634c5e0bb957317fe0
alextebbitt <alex.tebbitt87@gmail.com>
2022-04-29 11:56:59 +0200

    javascript alert message

--- javascript.js
+++ javascript.js
@@ -2,6 +2,11 @@ function onsubmit(e) {
     e.preventDefault();
     if(nombre.value=== ""|| email.value==="" || exampleInputPassword1.value=== ""||
      exampleInputPassword2.value==="") {
-        
+       console.log("Porfavor rellenar todo los campos") ;
+    } else {
+        msg.innerHTML="Exito";
     }
+    setTimeout(function(){
+        msg.innerHTML="";
+    },3000);
 }
