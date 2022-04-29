64c802fe52101a39c0a1f937b3cc861f3a6bef18
Yorch82 <jcampo1982@outlook.com>
2022-04-29 12:30:05 +0200

    nuevo form

--- javascript.js
+++ javascript.js
@@ -1,12 +1,35 @@
-function onsubmit(e) {
-    e.preventDefault();
-    if(nombre.value=== ""|| email.value==="" || exampleInputPassword1.value=== ""||
-     exampleInputPassword2.value==="") {
-       console.log("Porfavor rellenar todo los campos") ;
-    } else {
-        msg.innerHTML="Exito";
+const msg = document.getElementById('usuariosPintados')
+
+
+function enviarDatos() {     
+    let usuarios = [] || JSON.parse(localStorage.getItem("user")); 
+    let user = {
+        nombre: nombre.value,
+        correo: correo.value,
+        contraseña1: pwd1.value,
+        contraseña2: pwd2.value
     }
-    setTimeout(function(){
-        msg.innerHTML="";
-    },3000);
+    usuarios.push(user);
+    // console.log(usuarios)
+    localStorage.setItem("user", JSON.stringify(usuarios));
+    msg.innerHTML = 'correcto'    
+    // pintarUsuarios() 
 }
+
+
+
+
+
+
+// function onsubmit(e) {
+//     e.preventDefault();
+//     if(nombre.value=== ""|| email.value==="" || pwd1.value=== ""||
+//      pwd2.value==="") {
+//        console.log("Porfavor rellenar todo los campos") ;
+//     } else {
+//         msg.innerHTML="Exito";
+//     }
+//     setTimeout(function(){
+//         msg.innerHTML="";
+//     },3000);
+// }
