7b87b7424d94d05109867137d1b7ef79b4db43db
alextebbitt <alex.tebbitt87@gmail.com>
2022-04-29 11:39:36 +0200

    primer commit del projecto con Jorge

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function onsubmit(e) {
+    e.preventDefault();
+    if(nombre.value=== ""|| email.value==="" || exampleInputPassword1.value=== ""||
+     exampleInputPassword2.value==="") {
+        
+    }
+}
