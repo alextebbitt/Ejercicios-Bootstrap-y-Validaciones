97fc6a812bdf3d1045f4aa26422c161d709d83bb
Yorch82 <jcampo1982@outlook.com>
2022-04-30 11:24:02 +0200

    creada funcion mostrar carta

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
 myform.addEventListener("submit", onsubmit);
 const msg = document.getElementById("alert");
+const card = document.getElementById("card")
 
-<<<<<<< HEAD
-myform.addEventListener("submit", onsubmit)
-const msg = document.getElementById("alert")
-
-function enviarDatos() {     
+function enviarDatos() {         
     let usuarios = JSON.parse(localStorage.getItem("user")) || []; 
     let user = {
         nombre: nombre.value,
@@ -17,63 +14,44 @@ function enviarDatos() {
     localStorage.setItem("user", JSON.stringify(usuarios));     
 }
 
+function mostrarCarta() {
+    let data = JSON.parse(localStorage.getItem('user'));
+    for (let i = 0; i < data.length; i++) {
+        card.innerHTML += `<div class="card" style="width:400px">
+        <img class="card-img-top" src="./assets/hackerman.jpg" alt="Card image" style="width:75 %">
+        <div class="card-body">
+          <h4 class="card-title">${data[i].nombre}</h4>
+          <p class="card-text">${data[i].correo}</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>`
+    }
+}
 
- function onsubmit(e) {
-     e.preventDefault();
-     if (nombre.value === ""|| correo.value === "" || pwd1.value === ""|| pwd2.value==="") {
-        msg.innerHTML= `<div class="alert alert-warning">
-        <strong>Cuidado</strong> Debes rellenar todos los campos.
-        </div>`;
-     } else if (pwd1.value !== pwd2.value){
-        msg.innerHTML= `<div class="alert alert-danger">
-        <strong>Error!</strong> Contrasenas no coinciden.
-        </div>`
-     } else if (/(\w+?@\w+?\x2E.+)/.test(correo.value) !== true){
-        msg.innerHTML= `<div class="alert alert-danger">
-        <strong>Error!</strong> formato incorrecto.
-        </div>`
-     }
-     setTimeout(function(){
-         msg.innerHTML="";
-     },3000);     
- }
+
+function onsubmit(e) {
+    e.preventDefault();
+    if (nombre.value === ""|| correo.value === "" || pwd1.value === ""|| pwd2.value==="") {
+    msg.innerHTML= `<div class="alert alert-warning">
+    <strong>Cuidado</strong> Debes rellenar todos los campos.
+    </div>`;
+    } else if (pwd1.value !== pwd2.value){
+    msg.innerHTML= `<div class="alert alert-danger">
+    <strong>Error!</strong> Contrasenas no coinciden.
+    </div>`
+    } else if (/(\w+?@\w+?\x2E.+)/.test(correo.value) !== true){
+    msg.innerHTML= `<div class="alert alert-danger">
+    <strong>Error!</strong> formato incorrecto.
+    </div>`
+     } else {
+    msg.innerHTML= `<div class="alert alert-success">
+    <strong>Correcto!</strong> usuario creado.
+    </div>`
+    enviarDatos();
+    mostrarCarta();
+    }
+    setTimeout(function(){
+     msg.innerHTML="";
+    },3000);     
+}
 
  
-=======
-function onsubmit(e) {
-  e.preventDefault();
-  if (
-    nombre.value === "" ||
-    correo.value === "" ||
-    pwd1.value === "" ||
-    pwd2.value === ""
-  ) {
-    msg.innerHTML = `<div class="alert alert-warning">
-        <strong>Cuidado</strong> Debes rellenar todos los campos.
-        </div>`;
-  } else if (pwd1.value !== pwd2.value) {
-    msg.innerHTML = `<div class="alert alert-danger">
-        <strong>Error!</strong> Contrasenas no coinciden.
-        </div>`;
-  } else if (
-    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(correo.value)
-  ) {
-    msg.innerHTML = `<div class="alert alert-danger">
-        <strong>Error!</strong> No es el formato correcto.
-        </div>`;
-  } else {
-    let usuarios = JSON.parse(localStorage.getItem("user")) || [];
-    let user = {
-      nombre: nombre.value,
-      correo: correo.value,
-      contraseña1: pwd1.value,
-      contraseña2: pwd2.value,
-    };
-    usuarios.push(user);
-    localStorage.setItem("user", JSON.stringify(usuarios));
-}
-  setTimeout(function () {
-    msg.innerHTML = "";
-  }, 3000);
-}
->>>>>>> 3759ee330b44ab53419c0ab3086a21d7b23a4121
